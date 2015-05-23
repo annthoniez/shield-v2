2b8f3ee3a679bbb60939fecc8e579317c4ad2060
Nathawut Worakijlawan <topgunz.889@gmail.com>
2015-05-23 23:26:12 +0700

    Revert "login by username pass"
    This reverts commit 817b72a77ce8997aa5321c34fd51d823160e9ed9.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,10 +8,7 @@
 				<div class="panel-heading">Login</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
-						<div class="alert alert-danger alert-dismissible alert-popup fade in">
-							<button type="button" class="close" data-dismiss="alert">
-       							<span>&times;</span>
-      						</button>
+						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
