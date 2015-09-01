704ec8fc40c2f57bf66af6a56a6071909032708a
Nathawut Worakijlawan <topgunz.889@gmail.com>
2015-09-01 14:34:30 +0700

    Start Again

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,19 @@
-@extends('app')
+@extends('applogin')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
+<body class="login-page" style="background-color: #ecf0f5;">
+	<div class="col-xs-12 col-sm-4 col-sm-offset-4" style="margin-top: 4%;">
+		<div class="login-box-body">
+			<div class="row">
+				<div class="col-xs-12 text-center login-logo text-light-blue">
+						<img src="{{URL::to('assets/img/logo.png')}}" style="width: 40%; margin-top: 10px;">
+					</div>
+				<!-- -->
+				<div class="col-xs-12 login-box-right" style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px;">
+					<p style="text-align: center;">กองงานยุทธวิธีจัดระเบียบกำลังพิเศษแห่งมาตุภูมิ</p>
+					<p style="text-align: center;">ส่วนนี้เฉพาะเจ้าหน้าที่ - เข้าสู่ระบบ</p>
+						@if (count($errors) > 0)
+						<div class="alert alert-danger alert-dismissible alert-popup fade in">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
@@ -16,46 +21,37 @@
 								@endforeach
 							</ul>
 						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+						@endif
+					<form role="form" method="POST" action="{{ url('/auth/login') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Username</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="username" value="{{ old('username') }}">
-							</div>
+						<div class="form-group has-feedback">
+								<input type="text" class="form-control" name="username" placeholder="ชื่อผู้ใช้" value="{{ old('username') }}">
+
+								<span class="glyphicon glyphicon-user form-control-feedback"></span>
 						</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
+						<div class="form-group has-feedback">
+								<input type="password" class="form-control" name="password" placeholder="รหัสผ่าน"/>
+								<span class="glyphicon glyphicon-lock form-control-feedback"></span>
 						</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
+							<div class="row">
+								<div class="col-xs-7">
+									<div class="checkbox">
+										<label>
+											<input type="checkbox" name="remember" value="1" > จดจำการเข้าสู่ระบบ
+										</label>
+									</div>
+								</div>
+								<div class="col-xs-5">
+									<button type="submit" class="btn btn-primary btn-block">เข้าสู่ระบบ</button>
 								</div>
 							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
 					</form>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</body>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,8 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-<<<<<<< HEAD
-			<div class="alert alert-info alert-popup alert-dismissable fade in" data-dismiss="alert">
-				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-=======
->>>>>>> parent of 36b45c4... Update ++ Add menu
-		<div class="alert alert-info alert-dismissible alert-popup fade in">
-			<button type="button" class="close" data-dismiss="alert">
-				<span>&times;</span>
-			</button>
-			<strong>Success !</strong>
-			<ul>
-					You are logged in!
-<<<<<<< HEAD
-			</ul>
 
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-			</ul>
-=======
-			</ul>	
->>>>>>> parent of 36b45c4... Update ++ Add menu
-		</div>
 
-	</div>
-</div>
+
 @endsection
