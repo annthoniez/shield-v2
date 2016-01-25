cad2605c1380bcf7a290e420222de6298b6c5a42
Nathawut Worakijlawan <s7070038@kmitl.ac.th>
2016-01-25 21:03:00 +0700

    All Change

--- config/app.php
+++ config/app.php
@@ -194,7 +194,6 @@ return [
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
 		'HTML' 		=> 'Illuminate\Html\HtmlFacade',
-		'Form'		=> 'Illuminate\Html\FormFacade',
 
 	],
 
